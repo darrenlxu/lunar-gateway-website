74d13cd86c4cbb54743579157823f1c22560550b
Darren <65270652+darrenlxu@users.noreply.github.com>
2021-09-08 21:00:16 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,22 @@ app = Flask(__name__)
 
 @app.route("/")
 
+#Countdown page - "Gateway Countdown"
 def home():
     return render_template("home.html")
 
 @app.route("/more")
 
+#Second page - "More about Gateway"
 def more():
     return render_template("more.html")
 
 @app.route("/updates")
+
+#Third page - "All updates"
 def updates():
     return render_template("updates.html")
+
 
 if __name__ == "__main__":
     app.run()
